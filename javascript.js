e0be5d24615a367bfab7164932c80456c5c152fe
YuriTal <185655479+YurihTal@users.noreply.github.com>
2025-09-30 13:47:51 +0000

    Inputing playRound function skeleton

--- javascript.js
+++ javascript.js
@@ -1,7 +1,20 @@
 console.log("Hello, World!")
 
-const computerChoice = Math.floor(Math.random()*4)
+var computerChoice = Math.floor(Math.random()*4)
+var playerChoice = 0
 
+function playRound(computerChoice, playerChoice){
+    if (computerChoice = playerChoice) {
+        return ('It was a tie!');
+    }
+        else if (computerChoice > playerChoice){
+            return ('You Lose!');
+        }
+            else if (playerChoice > computerChoice){
+                return ('You Win!');
+            
+            }        
+}
 
-
+console.log(playerChoice)
 console.log(computerChoice)
