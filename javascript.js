e23503c0e0d2e3a62c67990286b083cde04f5d31
YuriTal <185655479+YurihTal@users.noreply.github.com>
2025-09-30 22:30:20 +0000

    Computer Logic & playRound Logic

--- javascript.js
+++ javascript.js
@@ -1,19 +1,24 @@
 console.log("Hello, World!")
 
-var computerChoice = Math.floor(Math.random()*4)
+const choices = ["rock","paper","scissors"]
+var computerChoice = choices[Math.floor(Math.random()*3)]
 var playerChoice = 0
 
 function playRound(computerChoice, playerChoice){
-    if (computerChoice = playerChoice) {
-        return ('It was a tie!');
+    if (computerChoice == playerChoice) {
+        return ('It Was A Tie!');
     }
-        else if (computerChoice > playerChoice){
-            return ('You Lose!');
+        else if (
+            (computerChoice == "rock" && playerChoice == "scissors") ||
+            (computerChoice == "paper" && playerChoice == "rock")
+            (computerChoice == "scissors" && playerChoice == "paper")
+        ){
+            return ('You Lost!');
         }
-            else if (playerChoice > computerChoice){
-                return ('You Win!');
+            else {
+                return ('You Won!');
             
-            }        
+            }      
 }
 
 console.log(playerChoice)
